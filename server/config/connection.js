720113e1e9dcedd109a15439c5e2ae28a1437de4
Mitchell Armstrong <mra0211@gmail.com>
2023-01-01 20:06:01 -0500

    Fixed the cart duplicating items when the page reloads and fixed an issue that wouldn't take the user to the stripe checkout page

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mernshopping",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/shop-shop",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   }
 );
 
